eb8aa90f20b6220afb9af66ee79aba3091c9b873
serban-alexandru <alexserb2009@yahoo.com>
2023-06-05 08:41:11 +0300

    .

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
-config.hosts = "railsrails.mhklpb.easypanel.host"
-
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.config.hosts << "railsrails.mhklpb.easypanel.host"
 
